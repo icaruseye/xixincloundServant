a3b3cb4b36c565f28b4523cd67f0d2f84e71f31f
liwenjin <921961677@qq.com>
2018-06-08 11:05:02 +0800

    路由模块化2

--- src/router/routerList.js
+++ src/router/routerList.js
@@ -5,10 +5,12 @@ import common from './routers/common'
 import message from './routers/message'
 import app from './routers/app'
 import user from './routers/user'
+import mission from './routers/mission'
 
 const routerList = [
   ...common,
   ...message,
+  ...mission,
   ...app,
   ...user
 ]
---
+++ src/router/routers/mission.js
@@ -0,0 +1,26 @@
+/**
+ * Created by lwj on 2018/6/8.
+ */
+import _import from '../_import'
+
+export default [
+  /**
+   * 医护端-任务
+   */
+  {
+     path: '/mission',
+     component: _import('/servant/mission/mission')
+  },
+  {
+    path: '/mission/receive/:id',
+    component: _import('/servant/mission/mission-toreceive')
+  },
+  {
+    path: '/mission/waitreceive/:id',
+    component: _import('/servant/mission/mission-waitreceive')
+  },
+  {
+    path: '/mission/detail/:id',
+    component: _import('/servant/mission/mission-detail')
+  }
+]
